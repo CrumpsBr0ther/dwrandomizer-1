511458b125f40e8479b5f91943e792e0bba381cb
Crumps Brother <>
2025-06-17 21:35:54 -0400

    NO_BUILDS user feedback
    Updated minimum str to 6 and mp to 8 (with spells). These values were previously always available to the runner by using specific builds to utilize early game penalty bonuses. These semi-important values becoming unavailable in NO_BUILDS could be seen as an issue.

--- common/dwr.c
+++ common/dwr.c
@@ -632,6 +632,13 @@ static void randomize_growth(dw_rom *rom)
             stats->hp = MAX(rom->stats[i-1].hp, stats->hp);
         }
     }
+
+    if (NO_BUILDS(rom)) {
+        for (i=0; i < 30; i++) {
+            stats = &rom->stats[i];
+            stats->str = MAX(stats->str, 6);
+        }
+    }
 }
 
 /**
@@ -682,8 +689,14 @@ static void randomize_spells(dw_rom *rom)
                 stats->spells |= 1 << (j + 8) % 16;
             }
         }
-        if (stats->spells) {
-            stats->mp = MAX(stats->mp, 6);
+        if (NO_BUILDS(rom)) {
+            if (stats->spells) {
+                stats->mp = MAX(stats->mp, 8);
+            }
+        } else {
+            if (stats->spells) {
+                stats->mp = MAX(stats->mp, 6);
+            }
         }
     }
 }
